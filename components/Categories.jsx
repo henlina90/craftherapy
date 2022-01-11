5e72a9fe58eb852553d6f51e8bfe26b8ae5ac44c
henlina90 <henlina90>
2022-01-11 10:46:37 -0800

    chg fontsize

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -16,7 +16,7 @@ const Categories = () => {
       <h3 className="text-xl mb-2 font-semibold pb-1 uppercase">Categories</h3>
       {categories.map((category, index) => (
         <Link key={index} href={`/category/${category.slug}`}>
-          <span className={`cursor-pointer block ${(index === categories.length - 1) } mb-2 text-gray-500 text-light text-sm`}>{category.name}</span>
+          <span className={`cursor-pointer block ${(index === categories.length - 1) } mb-2 text-gray-500 text-light text-md`}>{category.name}</span>
         </Link>
       ))}
     </div>
